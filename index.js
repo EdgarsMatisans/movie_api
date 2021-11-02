ab2a09435bb9c42f1f943b8eab157fc82303b013
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2021-11-03 00:21:24 +0100

    new additions

--- index.js
+++ index.js
@@ -10,42 +10,62 @@ app.use(express.static('public'));
  let MyMovies = [
     {
         title: 'Shrek',
+        genre: ['Animation', 'Family Film'],
+        director: ['Andrew Adamson', 'Vicky Jenson'],
         year: '2001'
     },
     {
         title: 'Shrek 2',
+        genre: ['Animation', 'Family Film'],
+        director: ['Andrew Adamson', 'Kelly Asbury', 'Conrad Vernon'],
         year: '2004'
     },
     {
         title: 'Shrek the Third',
+        genre: ['Animation', 'Family Film'],
+        director: ['Chris Miller'],
         year: '2007'
     },
     {
         title: 'Shrek Forever After',
+        genre : ['Animation', 'Family Film'],
+        director: ['Mike Mitchell'],
         yeat: '2010'
     },
     {
         title: 'How to Train Your Dragon',
+        genre: ['Adventure', 'Animation'],
+        director: ['Chris Sanders', 'Dean DeBlois'],
         year: '2010'
     },
     {
         title: 'How to Train Your Dragon 2',
+        genre: ['Adventure', 'Animation'],
+        director: ['Dean DeBlois'],
         year: '2014'
     },
     {
-        title: 'How to Train Your Dragon3',
+        title: 'How to Train Your Dragon 3',
+        genre: ['Adventure', 'Animation'],
+        director: ['Dean DeBlois'],
         year: '2019'
     },
     {
         title: 'The Hitman’s Bodyguard',
+        genre: ['Action', 'Comedy'],
+        director: ['Patrick Hughes'],
         year: '2017'
     },
     {
         title: 'Hitmans Wifes Bodyguard',
+        genre: ['Action', 'Comedy'],
+        director: ['Patrick Hughes'],
         year: '2021'
     },
     {   
         title: 'Deadpool',
+        genre: ['Action', 'Comedy'],
+        director: ['Tim Miller'],
         year: '2016'
     },
 ];
@@ -65,6 +85,49 @@ app.get('/secreturl', (req, res) => {
 app.get('/documentation', (req, res) => {
 	res.sendFile('public/documentation.html', { root: __dirname });
 });
+
+// Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user.
+app.get('/movies/:title', (req, res) => {
+    res.send(
+      'Successful GET request returning data about a single movies by title to the user.'
+    );
+  });
+
+  // Return data about a genre (description) by name/title (e.g., “Thriller”).
+app.get('/movies/:title/genre', (req, res) => {
+    res.send('Successful GET request returning data about a genre.');
+  });
+
+  // Return data about a director (bio, birth year, death year) by name.
+app.get('/movies/director/:name', (req, res) => {
+    res.send('Successful GET request returning data about a director.');
+  });
+
+  // Allow new users to register.
+app.post('/newUser', (req, res) => {
+    res.send('Successful POST request user was able to register.');
+  });
+
+  // Allow users to update their user info (username, password, email, date of birth).
+app.put('/newUser/:id/info', (req, res) => {
+    res.send('Successful PUT request user info updated.');
+  });
+
+  // Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/newUser/:id/favorites', (req, res) => {
+    res.send('Successful POST request movie has been added to favorites.');
+  });
+
+  // Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+app.delete('/newUser/:id/favorites', (req, res) => {
+    res.send(
+      'Successful DELETE request movie has been deleted from user list of favorites.');
+    });
+
+    // Allow existing users to deregister
+app.delete('/newUser', (req, res) => {
+    res.send('Successful DELETE request existing user has been deregistered.');
+  });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
