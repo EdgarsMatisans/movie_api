e860a09f19cc36d4e2d278bd2e6dda0ca105623e
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2021-11-24 21:39:50 +0100

    new additions

--- index.js
+++ index.js
@@ -1,81 +1,42 @@
 const express = require('express'),
-  morgan = require('morgan');
-
+      morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
+const bodyParser = require('body-parser');
 
+
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Director = Models.Director;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/test',
+  {useUnifiedTopology: true});
 
- let MyMovies = [
-    {
-        title: 'Shrek',
-        genre: ['Animation', 'Family Film'],
-        director: ['Andrew Adamson', 'Vicky Jenson'],
-        year: '2001'
-    },
-    {
-        title: 'Shrek 2',
-        genre: ['Animation', 'Family Film'],
-        director: ['Andrew Adamson', 'Kelly Asbury', 'Conrad Vernon'],
-        year: '2004'
-    },
-    {
-        title: 'Shrek the Third',
-        genre: ['Animation', 'Family Film'],
-        director: ['Chris Miller'],
-        year: '2007'
-    },
-    {
-        title: 'Shrek Forever After',
-        genre : ['Animation', 'Family Film'],
-        director: ['Mike Mitchell'],
-        yeat: '2010'
-    },
-    {
-        title: 'How to Train Your Dragon',
-        genre: ['Adventure', 'Animation'],
-        director: ['Chris Sanders', 'Dean DeBlois'],
-        year: '2010'
-    },
-    {
-        title: 'How to Train Your Dragon 2',
-        genre: ['Adventure', 'Animation'],
-        director: ['Dean DeBlois'],
-        year: '2014'
-    },
-    {
-        title: 'How to Train Your Dragon 3',
-        genre: ['Adventure', 'Animation'],
-        director: ['Dean DeBlois'],
-        year: '2019'
-    },
-    {
-        title: 'The Hitman’s Bodyguard',
-        genre: ['Action', 'Comedy'],
-        director: ['Patrick Hughes'],
-        year: '2017'
-    },
-    {
-        title: 'Hitmans Wifes Bodyguard',
-        genre: ['Action', 'Comedy'],
-        director: ['Patrick Hughes'],
-        year: '2021'
-    },
-    {   
-        title: 'Deadpool',
-        genre: ['Action', 'Comedy'],
-        director: ['Tim Miller'],
-        year: '2016'
-    },
-];
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my myFlix!');
 });
 
-app.get ('/movies', (req, res) => {
-    res.send(MyMovies);
+// Get a list of all the movies.
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movie) => {
+      res.status(201).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 app.get('/secreturl', (req, res) => {
@@ -88,25 +49,159 @@ app.get('/documentation', (req, res) => {
 
 // Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user.
 app.get('/movies/:title', (req, res) => {
-    res.send(
-      'Successful GET request returning data about a single movies by title to the user.'
-    );
-  });
+   Movies.findOne({Title: req.params.title})
+   .then((movie) => {
+    res.json(movie); 
+   })
+   .catch((err) => {
+     console.error(err);
+     res.status(500).send("Error: " + err);
+   });     
+    });
 
   // Return data about a genre (description) by name/title (e.g., “Thriller”).
-app.get('/movies/:title/genre', (req, res) => {
-    res.send('Successful GET request returning data about a genre.');
-  });
+app.get('/movies/genre/:name', (req, res) => {
+  Movies.findOne({"Genre.Name" : req.params.name})
+    .then((genre) => {
+      res.json(genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
   // Return data about a director (bio, birth year, death year) by name.
-app.get('/movies/director/:name', (req, res) => {
-    res.send('Successful GET request returning data about a director.');
+  app.get('/movies/director/:name', (req, res) => {
+    Movies.findOne({"Director.Name" : req.params.name})
+      .then((director) => {
+        res.json(director);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
   });
 
+  // Get all users
+app.get("/users", (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({Username: req.params.Username})
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
   // Allow new users to register.
-app.post('/newUser', (req, res) => {
-    res.send('Successful POST request user was able to register.');
+  app.post('/users', (req, res) => {
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user); })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
   });
+
+  // Update a user's info, by username
+  app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({Username: req.params.Username}, {$set:
+      {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      }
+    },
+    {new: true}, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+  });
+
+  // Add a movie to a user's list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username}, {
+     $push: {FavoriteMovies: req.params.MovieID}
+   },
+   {new: true}, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// Allow users to remove a movie from their list of favorites
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username}, {
+     $pull: {FavoriteMovies: req.params.MovieID}
+   },
+   {new: true}, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+// Delete a user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({Username: req.params.Username})
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
   // Allow users to update their user info (username, password, email, date of birth).
 app.put('/newUser/:id/info', (req, res) => {
