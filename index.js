0a7b597e35b6914517ad345e4e76f0428c03c1e9
Edgars Matisans <80711384+EdgarsMatisans@users.noreply.github.com>
2021-11-26 23:17:53 +0100

    new additions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express'),
 const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 
 
 const Models = require('./models.js');
@@ -159,26 +160,47 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
 });
 
   // Allow new users to register.
-  app.post('/users', (req, res) => {
-    Users.findOne({ Username: req.body.Username })
+  app.post('/users',
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+
+  // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
       .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + 'already exists');
-        } else {
-          Users
-            .create({
-              Username: req.body.Username,
-              Password: req.body.Password,
-              Email: req.body.Email,
-              Birthday: req.body.Birthday
-            })
-            .then((user) =>{res.status(201).json(user); })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-          });
-        }
-      })
+          if (user) {
+            //If the user is found, send a response that it already exists
+            return res.status(400).send(req.body.Username + ' already exists');
+          } else {
+            Users
+              .create({
+                Username: req.body.Username,
+                Password: hashedPassword,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+              })
+              .then((user) => { res.status(201).json(user); })
+              .catch((error) => {
+                console.error(error);
+                res.status(500).send('Error: ' + error);
+              });
+          }
+        })
       .catch((error) => {
         console.error(error);
         res.status(500).send('Error: ' + error);
@@ -260,7 +282,7 @@ app.use((err, req, res, next) => {
   });
 
 
-const port = process.env.PORT || 8080;
-    app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-    });
+  const port = process.env.PORT || 8080;
+  app.listen(port, '0.0.0.0',() => {
+   console.log('Listening on Port ' + port);
+  });
